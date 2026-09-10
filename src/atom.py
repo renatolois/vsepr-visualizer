23649bafc3c1a87acfbb00d32eff1d2d5a153a54
Renato Lóis <153780029+renatolois@users.noreply.github.com>
2026-09-10 11:31:06 +0000

    Update atom.py

--- src/atom.py
+++ src/atom.py
@@ -1,39 +1,68 @@
 from glm import vec4
 from polygons.sphere import Sphere
+from electron_cloud import ElectronCloud
+from light import Light
+from camera import Camera
+from renderer import Renderer
+from transform import Transform
 
 
 class Atom:
     def __init__(
         self,
-        tag_id: int,
-        tag_size: float,
-        radius_constant_factor: float = 0.7,
-        sphere_lat_level: int = 3,
-        sphere_lon_level: int = 3,
-        sphere_color: vec4 = vec4(1.0, 0.0, 0.0, 1.0)
+        nucleus_radius: float = 0.7,
+        nucleus_color: vec4 = vec4(1.0, 0.0, 0.0, 1.0),
+        nucleus_level: int = 4,
+        electron_radius: float = 0.2,
+        electron_level: int = 2,
+        electron_cloud_color: vec4 = vec4(6.0, 7.0, 0.0, 1.0),
+        electron_cloud_radius: float = 3,
+        electron_cloud_num_positions: int = 60,
+        electron_cloud_speed: float = 2.0,
+        electron_cloud_num_electrons: float = 4,
+        decay_rate: float = 5.0  # obsolet param
     ) -> None:
-        self.tag_id: int = tag_id
-        self.tag_size: float = tag_size
-        self.radius_constant_factor: float = radius_constant_factor
+        self.nucleus_radius = nucleus_radius
+        self.nucleus_color = nucleus_color
+        self.nucleus_level = nucleus_level
+        self.electron_radius = electron_radius
+        self.electron_level = electron_level
+        self.electron_cloud_color = electron_cloud_color
+        self.electron_cloud_num_electrons = electron_cloud_num_electrons
+        self.electron_cloud_num_positions = electron_cloud_num_positions
+        self.electron_cloud_speed = electron_cloud_speed
+        self.decay_rate = decay_rate
+        self.electron_cloud_radius = electron_cloud_radius
 
-        self.atom_sphere: Sphere = Sphere(
-            tag_size * radius_constant_factor,
-            sphere_lat_level,
-            sphere_lon_level,
-            sphere_color
+        self.atom = Sphere (
+            radius=self.nucleus_radius,
+            lat_level=self.nucleus_level,
+            lon_level=self.nucleus_level,
+            sphere_color=self.nucleus_color
         )
 
-    def get_tag_size(self) -> float:
-        return self.tag_size
+        self.electron_cloud = ElectronCloud (
+            num_electrons=self.electron_cloud_num_electrons,
+            radius=self.electron_cloud_radius,
+            electron_radius=self.electron_radius,
+            color=self.electron_cloud_color,
+            speed=self.electron_cloud_speed,
+            num_positions=self.electron_cloud_num_positions,
+            decay_rate=self.decay_rate
+        )
 
-    def set_tag_id(self, tag_id: int) -> None:
-        self.tag_id = tag_id
+        self.atom.sphere_entity.add_child (
+            self.electron_cloud.electron_cloud_entity
+        )
 
-    def get_tag_id(self) -> int:
-        return self.tag_id
+    def get_transform(self) -> Transform:
+        return self.atom.sphere_entity.get_transform()
 
-    def get_color(self) -> vec4:
-        return self.atom_sphere.get_color()
+    def set_transform(self, transform: Transform) -> None:
+        self.atom.sphere_entity.set_transform(transform)
 
-    def set_color(self, color: vec4) -> None:
-        self.atom_sphere.set_color(color)
+    def update_positions(self, delta_time):
+        self.electron_cloud.update_positions(delta_time)
+
+    def render(self, renderer: Renderer, camera: Camera, light: Light) -> None:
+        renderer.render_entity(camera, self.atom.sphere_entity, light)
